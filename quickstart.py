3a1f91235b54dd7977f43fe31a2ea9b8a3d32f37
MarcM <97142000+Lilred83@users.noreply.github.com>
2024-02-08 11:18:13 -0800

    Email Retrieval From Gmail
    Retrieves email subject and text in label you specify

---
+++ quickstart.py
@@ -0,0 +1,112 @@
+# Install in terminal -> pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+
+# Labels
+INBOX = "INBOX"
+CHAT = "CHAT"
+SENT = "SENT"
+IMPORTANT = "IMPORTANT"
+TRASH = "TRASH"
+DRAFT = "DRAFT"
+SPAM = "SPAM"
+CATEGORY_FORUMS = "CATEGORY_FORUMS"
+CATEGORY_UPDATES = "CATEGORY_UPDATES"
+CATEGORY_PERSONAL = "CATEGORY_PERSONAL"
+CATEGORY_PROMOTIONS = "CATEGORY_PROMOTIONS"
+CATEGORY_SOCIAL = "CATEGORY_SOCIAL"
+STARRED = "STARRED"
+UNREAD = "UNREAD"
+
+
+def extractEmails(service, label_id):
+    try:
+        # Call the Gmail API
+        # Retrieve messages from given label
+
+        results = service.users().messages().list(
+            userId="me", labelIds=[label_id]).execute()
+        messages = results.get("messages", [])
+
+        # Retrieve and print the content of each message
+        for message in messages:
+            msg = service.users().messages().get(
+                userId="me", id=message["id"]).execute()
+            msg = service.users().messages().get(
+                userId="me", id=message["id"]).execute()
+            headers = msg["payload"]["headers"]
+            # Find the subject header in the email headers
+            subject = next(
+                (header["value"] for header in headers if header["name"] == "Subject"), None)
+            if subject:
+                print("Subject:", subject)
+            else:
+                print("Subject not found")
+
+            print("Message ID:", message["id"])
+            # Print snippet of the message
+            print("Message Text:", msg["snippet"])
+            print("\n")
+
+    except HttpError as error:
+        # TODO: Handle errors from Gmail API.
+        print(f"An error occurred: {error}")
+
+
+def main():
+    # Creates List of the user's Gmail labels.
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        # Call the Gmail API
+        service = build("gmail", "v1", credentials=creds)
+        results = service.users().labels().list(userId="me").execute()
+        labels = results.get("labels", [])
+
+        if not labels:
+            print("No labels found.")
+            return
+        print("Labels:")
+
+        for label in labels:
+
+            # Extract emails from Inbox
+            if label["name"] == INBOX:
+                extractEmails(service, label["id"])
+                break
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f"An error occurred: {error}")
+
+
+if __name__ == "__main__":
+    main()
